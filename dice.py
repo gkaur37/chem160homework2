d1be9cdb245f14ae2eb43d28f74b2fefd45b636c
gkaur37 <54698652+gkaur37@users.noreply.github.com>
2019-09-15 23:55:13 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,18 @@
+from random import choices
+ntrials = 10000
+player1wins = 0
+for i in range(ntrials):
+    num_dice1 = 2
+    num_dice2 = 3
+    dice1 = choices(range(1, 7), k= num_dice1)
+    if dice1[0] == dice1[1]:
+        player1wins += 1
+
+    dice2 = choices(range(1, 7), k= num_dice2)
+    dice2.sort(reverse=True)
+    sum1 = dice1[0]+dice1[1]
+    sum2 = dice2[0]+dice2[1]
+    if sum1 >= sum2:
+        player1wins +=1
+    avg_rolls = player1wins/ ntrials
+print(avg_rolls)
